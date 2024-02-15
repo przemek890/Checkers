b3afba9c8647ee26b2f9f4b77f2ee5c0d820d7a2
przemek890 <przemek.899@wp.pl>
2024-02-15 01:44:09 +0100

    little corrections

--- inc/Mode.h
+++ inc/Mode.h
@@ -12,6 +12,6 @@
 double SCR_W = static_cast<double>(sf::VideoMode::getDesktopMode().width);
 double SCR_H = static_cast<double>(sf::VideoMode::getDesktopMode().height);
 double res = (sqrt(pow(SCR_H,2) + pow(SCR_W,2))) / (sqrt(pow(2000,2) + pow(1600,2)));
-double resolution_mode = res + 0.25 * res;
+double resolution_mode = res / (0.5*res);
 //---------------------
 #endif //CHECKERS_MODE_H
